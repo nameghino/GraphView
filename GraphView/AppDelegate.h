3a50aff74b4664dfc5a1f45199a3b4954b2d0fd5
Nicolas Ameghino <nicolas.ameghino@globant.com>
2015-01-19 14:51:23 -0300

    Initial Commit

---
+++ GraphView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraphView
+//
+//  Created by Nicolas Ameghino on 1/19/15.
+//  Copyright (c) 2015 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GraphView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GraphView
+//
+//  Created by Nicolas Ameghino on 1/19/15.
+//  Copyright (c) 2015 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
