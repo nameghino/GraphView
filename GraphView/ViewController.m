483d95ece15a2dcd27ca422ab2697b0247e6cf8e
Nicolas Ameghino <nicolas.ameghino@globant.com>
2015-01-20 15:59:51 -0300

    Initial version of the test view controller

--- GraphView/ViewController.m
+++ GraphView/ViewController.m
@@ -7,21 +7,63 @@
 //
 
 #import "ViewController.h"
+#import "GraphView.h"
+#import "BubbleNodeView.h"
 
-@interface ViewController ()
-
+@interface ViewController () <GraphViewDelegate>
+@property(nonatomic, strong) GraphView *graphView;
+@property(nonatomic, strong) id<Graph> graph;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    GraphView *gv = [[GraphView alloc] init];
+    [self.view addSubview:gv];
+    NSDictionary *views = NSDictionaryOfVariableBindings(gv);
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[gv]|"
+                                                                      options:NSLayoutFormatAlignAllCenterY
+                                                                      metrics:nil
+                                                                        views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[gv]|"
+                                                                      options:NSLayoutFormatAlignAllCenterX
+                                                                      metrics:nil
+                                                                        views:views]];
+    _graphView = gv;
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    id<Graph> graph = [self createGraph];
+    [_graphView setGraph:graph];
+    [_graphView layoutIfNeeded];
+}
+
+
+-(id<Graph>) createGraph {
+    return nil;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - GraphViewDelegate
+-(NSInteger)numberOfNodesInGraphView:(GraphView *)graphView {
+    return [self.graph.nodes count];
+}
+
+-(NSString *)keyForFirstNodeInGraphView:(GraphView *)graphView {
+    return @"A";
+}
+
+-(GraphNodeView *)graphView:(GraphView *)graphView viewForNode:(id<GraphNode>)node {
+    //TODO: add GraphNodeView reuse
+    GraphNodeView *nodeView = [[BubbleNodeView alloc] init];
+    nodeView.node = node;
+    return nodeView;
 }
 
 @end
